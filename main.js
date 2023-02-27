2833e9cf4da7a90ff17a85ecd559b302311c05b9
PedroEtga <126345852+PedroEtga@users.noreply.github.com>
2023-02-27 12:32:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+function entrar(){
+    botao.style.background = '#F3C61F'
+    botao.style.color = '#514988'
+}
+
+function sair(){
+    botao.style.background ='#514988'
+    botao.style.color = '#F3C61F'
+}
+
+function big(qui) {
+    qui.style.height = "350px";
+    qui.style.width = "254px";
+}
+
+function normal(qui) {
+    qui.style.height = "300px";
+    qui.style.width = "204px";
+}
+
+function big(sex) {
+    sex.style.height = "350px";
+    sex.style.width = "254px";
+}
+
+function normal(sex) {
+    sex.style.height = "300px";
+    sex.style.width = "204px";
+}
+
+function big(sab) {
+    sab.style.height = "350px";
+    sab.style.width = "254px";
+}
+
+function normal(sab) {
+    sab.style.height = "300px";
+    sab.style.width = "204px";
+}
+
+function big(dom) {
+    dom.style.height = "350px";
+    dom.style.width = "254px";
+}
+
+function normal(dom) {
+    dom.style.height = "300px";
+    dom.style.width = "204px";
+}
---
+++ script.js
@@ -0,0 +1,29 @@
+const dia = document.getElementById('dias')
+const hora = document.getElementById('horas')
+const minuto = document.getElementById('minutos')
+const segundo = document.getElementById('segundos')
+
+const chegada = "22 Dec 2023"
+// Coloquei outra data para que o timer não fique zerado 
+
+function countDown(){
+    const dataFinal = new Date(chegada)
+    const hoje = new Date()
+    
+    const segTotal = ( dataFinal - hoje)/1000;
+
+    const finalDias = Math.floor(segTotal /60 / 60 / 24);
+    const finalHoras = Math.floor(segTotal /60 /60) % 24;
+    const finalMinutos = Math.floor(segTotal / 60) % 60;
+    const finalSegundos = Math.floor(segTotal) % 60 ;
+
+    dia.innerHTML = finalDias +("D")
+    hora.innerHTML = formatoTempo(finalHoras)+ ("H")
+    minuto.innerHTML = formatoTempo(finalMinutos)+("M")
+    segundo.innerHTML = formatoTempo(finalSegundos)+("S")
+}
+function formatoTempo(tempo){
+    return tempo < 10? `0${tempo}`: tempo;
+}
+countDown();
+setInterval (countDown,1000)
